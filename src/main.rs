99311ac03a2ef0437de988c654c8713f70a172ff
kody.low <kodylow7@gmail.com>
2024-10-30 18:14:29 -0700

    fix: fixed rust version issue with lock

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use tracing::info;
+
 mod cli;
 mod decode;
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
-    // tracing_subscriber::fmt()
-    //     .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-    //     .init();
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
 
     let args = cli::Args::parse();
 
-    println!(
+    info!(
         "Command: {:?}, Filename: {:?}",
         args.command,
         args.bencoded_file.display()
