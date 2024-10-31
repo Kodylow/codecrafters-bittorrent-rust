4c2fef28cad441db172037e15e4ac82ba55d53b7
kody.low <kodylow7@gmail.com>
2024-10-30 18:10:47 -0700

    fix: remove tracing, doesnt work with it

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use tracing::info;
-
 mod cli;
 mod decode;
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
-    tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .init();
+    // tracing_subscriber::fmt()
+    //     .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+    //     .init();
 
     let args = cli::Args::parse();
 
-    info!(
+    println!(
         "Command: {:?}, Filename: {:?}",
         args.command,
         args.bencoded_file.display()
